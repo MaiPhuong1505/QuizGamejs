bc52498519a7b62f62d2c475f02b9a2af91a125b
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2023-07-11 15:33:15 +0700

    change env variable for react

--- client/src/socketClient.js
+++ client/src/socketClient.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
-const URL = process.env.SOCKET_URL || 'http://localhost:5001';
+const URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5001';
 export const socket = io(URL);
